eae6111d8b30667bd282b91c84afdab140e0ac46
Vaibhav17031996 <vaibhavpatwal3@gmail.com>
2024-04-11 13:03:37 +0530

    assignment done

--- components/LanguageSwitcher.js
+++ components/LanguageSwitcher.js
@@ -1,17 +1,22 @@
-"use client"
-import {  } from '../contexts/LanguageContext';
+"use client";
+import {} from "../contexts/LanguageContext";
+import { useContext } from "react";
+import { LanguageContext } from "../contexts/LanguageContext";
 
 const LanguageSwitcher = () => {
-    
+  const languageState = useContext(LanguageContext);
 
-    return (
-        <select value={language} >
-            <option value="en">English</option>
-            <option value="fr">French</option>
-            <option value="es">Spanish</option>
-            {/* Add more languages as needed */}
-        </select>
-    );
+  return (
+    <select
+      value={languageState.language}
+      onChange={(e) => languageState.setLanguage(e.target.value)}
+    >
+      <option value="en">English</option>
+      <option value="fr">French</option>
+      <option value="es">Spanish</option>
+      {/* Add more languages as needed */}
+    </select>
+  );
 };
 
 export default LanguageSwitcher;
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,31 @@
-"use client"
-import  from '../components/LanguageSwitcher';
-import {  } from '../contexts/LanguageContext';
+"use client";
+import LanguageSwitcher from "../components/LanguageSwitcher";
+import {} from "../contexts/LanguageContext";
+import { useContext } from "react";
+import { LanguageContext } from "../contexts/LanguageContext";
 
 const Home = () => {
-    
+  const languageState = useContext(LanguageContext);
 
-    const content = {
-        en: {
-            welcome: 'Welcome to Next.js!'
-        },
-        fr: {
-            welcome: 'Bienvenue à Next.js!'
-        },
-        es: {
-            welcome: '¡Bienvenido a Next.js!'
-        }
-        // ... add other languages and translations as needed
-    };
+  const content = {
+    en: {
+      welcome: "Welcome to Next.js!",
+    },
+    fr: {
+      welcome: "Bienvenue à Next.js!",
+    },
+    es: {
+      welcome: "¡Bienvenido a Next.js!",
+    },
+    // ... add other languages and translations as needed
+  };
 
-    return (
-        <div>
-            <h1>{}</h1>
-            <LanguageSwitcher />
-        </div>
-    );
+  return (
+    <div>
+      <h1>{content[languageState.language].welcome}</h1>
+      <LanguageSwitcher />
+    </div>
+  );
 };
 
 export default Home;
